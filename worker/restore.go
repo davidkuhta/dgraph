7de7f4bd30d67d09731d52a25039533c09a96dc8
NamanJain8 <jnaman806@gmail.com>
2021-01-31 19:26:40 +0530

    no need to drop the schema at restore

--- worker/restore.go
+++ worker/restore.go
@@ -105,14 +105,6 @@ func loadFromBackup(db *badger.DB, r io.Reader, restoreTs uint64, preds predicat
 		return 0, errors.Wrapf(err, "cannot apply DROP operations while loading backup")
 	}
 
-	// Delete schemas and types. Each backup file should have a complete copy of the schema.
-	if err := db.DropPrefix([]byte{x.ByteSchema}); err != nil {
-		return 0, err
-	}
-	if err := db.DropPrefix([]byte{x.ByteType}); err != nil {
-		return 0, err
-	}
-
 	loader := db.NewKVLoader(16)
 	var maxUid uint64
 	for {
